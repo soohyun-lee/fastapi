b60142048a9f6b74a7de44d5a1423d7f55143167
soohyun-lee <soohyun527@gmail.com>
2021-08-30 18:06:02 +0900

    first commit

---
+++ sql_app/main.py
@@ -0,0 +1,15 @@
+from typing import List
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
